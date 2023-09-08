226653421d5c038b5cd7d18677891ae9ebcbcb36
wongeo <29992379@qq.com>
2023-09-08 10:38:20 +0800

    update

--- video/src/androidTest/java/com/feng/video/ExampleInstrumentedTest.java
+++ video/src/androidTest/java/com/feng/video/ExampleInstrumentedTest.java
@@ -1,6 +1,5 @@
 package com.feng.video;
 
-import android.app.Instrumentation;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -10,8 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-
-import com.feng.video.util.DiscoverNetIpUtil;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,20 +23,4 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.feng.video.test", appContext.getPackageName());
     }
-
-    @Test
-    public void findIp() {
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        // 获取Instrumentation实例
-        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-
-        // 模拟和授予权限
-        instrumentation.getUiAutomation().executeShellCommand("pm grant com.feng.fplayer android.permission.ACCESS_WIFI_STATE");
-
-        // 调用需要权限的API
-        String ip = DiscoverNetIpUtil.getIpAddress(appContext);
-        assertNotNull(ip);
-    }
-
 }
