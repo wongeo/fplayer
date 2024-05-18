751a2b321c3fdbc12efd54ce53bbd194e0bbacb6
wongeo <29992379@qq.com>
2024-05-18 19:22:26 +0800

    update

--- app/src/main/java/com/feng/player/ui/MediaList.kt
+++ app/src/main/java/com/feng/player/ui/MediaList.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -33,9 +35,10 @@ fun MediaList(listViewModel: MediaListViewModel = viewModel()) {
     LaunchedEffect(key1 = Unit) {
         listViewModel.fetchData(context)
     }
-    LazyVerticalGrid(
-        cells = GridCells.Fixed(2), // 设置列数
-        verticalArrangement = Arrangement.spacedBy(8.dp), // 设置元素之间的垂直间距
+
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        verticalItemSpacing = 8.dp,
         horizontalArrangement = Arrangement.spacedBy(8.dp) // 设置元素之间的水平间距
     ) {
         items(listViewModel.list) { item ->
